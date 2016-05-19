5d295064b9e24ac73ef481041713007a83b753b2
Devin Shively <devin.shively@dailymotion.com>
2016-05-18 17:34:13 -0700

    Initial Commit

---
+++ SampleVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleVideoPlayer
+//
+//  Created by Devin SHIVELY on 5/18/16.
+//  Copyright © 2016 devinshively. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
